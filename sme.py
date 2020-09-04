677261240204e19dcd71ca7750f8c1b0635c473a
albertjlay <albertjlay@gmail.com>
2020-09-04 11:10:32 +0700

    Improved UI and anticipates bad input Removes navframe for better ease of use. Renames variables that are in the normal section to prevent future mixups. Anticipates two bad inputs: no input and not integers.

--- sme.py
+++ sme.py
@@ -13,80 +13,92 @@ header2 = tkfont.Font(size = 24, family = "Impact")
 inline = tkfont.Font(size = 14, family = "Helvetica")
 equation = tkfont.Font(size = 14, family = "Baskerville")
 
-
 #Function to calculate normal probability
-def normal ():
-    #If variance was inputted, change to SD first before calculaton. If SD, store SD as float.
-    if sd_var.get() == "Variance":
-        sigma = math.sqrt(float(sd_entry.get()))
-    else:
-        sigma = float(sd_entry.get())
+def normal (x):
+    if x == 0:
+        #If variance was inputted, change to SD first before calculaton. If SD, store SD as float.
+        if norm_sd_var.get() == "Variance":
+            sigma = math.sqrt(float(norm_sd_entry.get()))
+        else:
+            sigma = float(norm_sd_entry.get())
 
-    #Determines whether probability being searched is less than or more than X. Then calculate probability.
-    if operation_var.get() == "<":
-        probability = (norm.cdf (float(x_entry.get()), loc = float(mean_entry.get()), scale = sigma))
-        result_label.config(text = "The probability that X is lower than " + x_entry.get() + " is " + str(round(probability,3)))
-    elif operation_var.get() == ">":
-        probability = 1 - (norm.cdf (float(x_entry.get()), loc = float(mean_entry.get()), scale = sigma))
-        result_label.config(text = "The probability that X is more than " + x_entry.get() + " is " + str(round(probability,3)))
-
-
-
-#Frame for main navigation. Left side
-navframe = LabelFrame (root)
-navframe.grid (row = 0, column = 0)
-
-#Inside of navframe
-mainheader = Label (navframe, text = "STATS MADE EASY", width = 20, font = header1, fg = "#f2c022")
-instruction = Label (navframe, text = "Hi, fellow students!\nUse this programs as a tool to help you study for your A-Level Mathematics: Statistics Module.", width = 40, height = 5, font = inline, fg = "black", wraplength = 310)
-binombutton = Button (navframe, text = "BINOMIAL", width = 31, font = header2, height = 2)
-poissonbutton = Button (navframe, text = "POISSON", width = 31, font = header2, height = 2)
-normalbutton = Button (navframe, text = "NORMAL", width = 31, font = header2, height = 2)
-
-mainheader.grid(row = 0, column = 0)
-instruction.grid (row = 1, column = 0)
-binombutton.grid (row = 2, column = 0, pady = 5)
-poissonbutton.grid (row = 3, column = 0, pady = 5)
-normalbutton.grid (row = 4, column = 0, pady = 5)
-
-#Frame for calculation. Right side.
-calcframe = LabelFrame (root)
-calcframe.grid (row = 0, column = 1, padx = 80)
-
-#Inside of calcframe
-sd_var = StringVar()
-sd_var.set("Standard Deviation")
-
-operation_var = StringVar()
-operation_var.set("<")
-
-header_normal = Label (calcframe, text = "Normal Distribution", width = 20, font = header1, fg = "#14a795")
-mean_label = Label (calcframe, text = "Mean", font = equation)
-mean_entry = Entry (calcframe, width = 10)
-sd_dropdown = OptionMenu (calcframe, sd_var, "Standard Deviation", "Variance")
-sd_dropdown.config (font = equation, width = 15, anchor = W, padx = 10)
-sd_entry = Entry (calcframe, width = 10)
-x_label = Label (calcframe, text = "X", font = equation)
-x_entry = Entry (calcframe, width = 10)
-normal_submit = Button (calcframe, text = "SUBMIT", font = inline, command = normal, width = 20)
-operation_label1 = Label (calcframe, text = "=", font = equation)
-operation_label2 = Label (calcframe, text = "=", font = equation)
-operation_dropdown = OptionMenu (calcframe, operation_var, "<", ">")
-result_label = Label (calcframe, text = "Result will be shown here.", font = inline)
+            #Determines whether probability being searched is less than or more than X. Then calculate probability.
+            if norm_operation_var.get() == "<":
+                probability = (norm.cdf (float(norm_x_entry.get()), loc = float(norm_mean_entry.get()), scale = sigma))
+                norm_result.config(text = "The probability that X is lower than " + norm_x_entry.get() + " is " + str(round(probability,3)))
+            elif norm_operation_var.get() == ">":
+                probability = 1 - (norm.cdf (float(norm_x_entry.get()), loc = float(norm_mean_entry.get()), scale = sigma))
+                norm_result.config(text = "The probability that X is more than " + norm_x_entry.get() + " is " + str(round(probability,3)))
 
 
-header_normal.grid (row = 0, column = 0, columnspan = 3, pady = (0, 20))
-mean_label.grid (row = 1, column = 0, sticky = W, padx = 3)
-mean_entry.grid (row = 1, column = 2)
-sd_dropdown.grid (row = 2, column = 0, sticky = W, padx = 3)
-sd_entry.grid (row = 2, column = 2)
-x_label.grid (row = 3, column = 0, sticky = W, padx = 3)
-x_entry.grid (row = 3, column = 2, pady = 10)
-normal_submit.grid (row = 4, column = 0, columnspan = 3)
-operation_label1.grid (row = 1, column = 1)
-operation_label2.grid (row = 2, column = 1)
-operation_dropdown.grid (row = 3, column = 1)
-result_label.grid(row = 5, column = 0, columnspan = 2)
+def normal_errorcheck():
+    #x is variable that will be returned to the normal function to indicate whether an error occurred. 0 means no error.
+    x = 0
+    for i in norm_entries:
+        #Checks for Error 1 : No input was given
+        if i.get().strip() == "":
+            i.insert (0,"Error 1: Please insert a parameter.")
+            x = 1
+        else:
+            #Checks for error 2: Input was not a number.
+            try:
+                float(i.get().strip())
+            except:
+                i.delete (0, END)
+                i.insert (0,"Error 2: Please insert a valid number.")
+                x = 2
+    normal (x)
+
+
+#Frame for Normal distribution section.
+normalframe = LabelFrame (root)
+normalframe.grid (row = 0, column = 1, padx = 80)
+
+#Inside of normalframe
+#Stores whether user chooses SD or variance.
+norm_sd_var = StringVar()
+norm_sd_var.set("Standard Deviation")
+
+#Stores which operation user chooses.
+norm_operation_var = StringVar()
+norm_operation_var.set("<")
+
+norm_header = Label (normalframe, text = "Normal Distribution", width = 20, font = header1, fg = "#14a795")
+
+norm_mean_label = Label (normalframe, text = "Mean", font = equation)
+norm_mean_entry = Entry (normalframe, width = 25)
+
+norm_sd_dropdown = OptionMenu (normalframe, norm_sd_var, "Standard Deviation", "Variance")
+norm_sd_dropdown.config (font = equation, width = 15, anchor = W, padx = 10)
+norm_sd_entry = Entry (normalframe, width = 25)
+
+norm_x_label = Label (normalframe, text = "X", font = equation)
+norm_x_entry = Entry (normalframe, width = 25)
+
+norm_submit = Button (normalframe, text = "CALCULATE", font = inline, command = normal_errorcheck, width = 20)
+
+norm_operation_label1 = Label (normalframe, text = "=", font = equation)
+norm_operation_label2 = Label (normalframe, text = "=", font = equation)
+norm_operation_dropdown = OptionMenu (normalframe, norm_operation_var, "<", ">")
+
+norm_result = Label (normalframe, text = "Result will be shown here.", font = inline)
+
+#Tuple for all norm_entry fields
+norm_entries = (norm_mean_entry, norm_sd_entry, norm_x_entry)
+
+#Places Normal widget on screen
+norm_header.grid (row = 0, column = 0, columnspan = 3, pady = (0, 20))
+norm_mean_label.grid (row = 1, column = 0, sticky = W, padx = 3)
+norm_mean_entry.grid (row = 1, column = 2)
+norm_sd_dropdown.grid (row = 2, column = 0, sticky = W, padx = 3)
+norm_sd_entry.grid (row = 2, column = 2)
+norm_x_label.grid (row = 3, column = 0, sticky = W, padx = 3)
+norm_x_entry.grid (row = 3, column = 2, pady = 10)
+norm_submit.grid (row = 4, column = 0, columnspan = 3)
+norm_operation_label1.grid (row = 1, column = 1)
+norm_operation_label2.grid (row = 2, column = 1)
+norm_operation_dropdown.grid (row = 3, column = 1)
+norm_result.grid(row = 5, column = 0, columnspan = 3)
 
 
 
